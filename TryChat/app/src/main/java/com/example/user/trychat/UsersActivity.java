c4c59dab1db344676cc2e94668d02ae1e4fe3f22
Ryan Lin <servant1001@gmail.com>
2018-06-11 17:11:05 +0800

    Update
    2018.6.11

--- TryChat/app/src/main/java/com/example/user/trychat/UsersActivity.java
+++ TryChat/app/src/main/java/com/example/user/trychat/UsersActivity.java
@@ -8,15 +8,21 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -25,10 +31,11 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class UsersActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
-
     private RecyclerView mUsersList;
-
     private DatabaseReference mUsersDatabase;
+
+    private EditText Search_friend_et;
+    private Button Search_friend_btn,Search_clear_friend_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +47,81 @@ public class UsersActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("All Users");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        Search_friend_et = findViewById(R.id.search_friend_et);
+        Search_friend_btn = findViewById(R.id.search_friend_btn);
+        Search_clear_friend_btn = findViewById(R.id.search_clear_friend_btn);
+
         mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
 
         mUsersList = (RecyclerView)findViewById(R.id.users_list);
         mUsersList.setHasFixedSize(true);
         mUsersList.setLayoutManager(new LinearLayoutManager(this));
 
+        Search_friend_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchUserName = Search_friend_et.getText().toString();
+
+                if (TextUtils.isEmpty(searchUserName)){
+                    Toast.makeText(UsersActivity.this, "Please enter friend's name  to search...",Toast.LENGTH_SHORT).show();
+                }
+                SearchFriends(searchUserName);
+            }
+        });
+
+        Search_clear_friend_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Search_friend_et.setText("");
+            }
+        });
     }
 
-    @Override
+    private void SearchFriends(String searchUserName){
+
+        Query searchPeopleAndFriend = mUsersDatabase.orderByChild("name")
+                .startAt(searchUserName).endAt(searchUserName + "\uf8ff");
+
+        FirebaseRecyclerOptions<Users> options = new FirebaseRecyclerOptions.Builder<Users>()
+                .setQuery(searchPeopleAndFriend, Users.class)
+                .build();
+
+        FirebaseRecyclerAdapter firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Users, UsersViewHolder>(options) {
+
+            @Override
+            protected void onBindViewHolder(@NonNull UsersViewHolder usersViewHolder, int position, @NonNull Users users) {
+                usersViewHolder.setDisplayName(users.getName());
+                usersViewHolder.setUserStatus(users.getStatus());
+                usersViewHolder.setUserImage(users.getThumb_image(), getApplicationContext());
+
+                final String user_id = getRef(position).getKey();
+
+                usersViewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent profileIntent = new Intent(UsersActivity.this, ProfileActivity.class);
+                        profileIntent.putExtra("from_user_id", user_id);
+                        startActivity(profileIntent);//夾帶user_id 傳到 ProfileActivity.class
+                    }
+                });
+            }
+
+            @Override
+            public UsersViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.users_single_layout, parent, false);
+
+                return new UsersViewHolder(view);
+            }
+        };
+
+        mUsersList.setAdapter(firebaseRecyclerAdapter);
+        firebaseRecyclerAdapter.startListening();
+    }
+
+
+    //正常顯示所有使用者
+    /*@Override
     protected void onStart() {
         super.onStart();
 
@@ -87,7 +160,7 @@ public class UsersActivity extends AppCompatActivity {
 
         mUsersList.setAdapter(firebaseRecyclerAdapter);
         firebaseRecyclerAdapter.startListening();
-    }
+    }*/
 
     public static class UsersViewHolder extends  RecyclerView.ViewHolder{
 
